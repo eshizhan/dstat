f703b21fc37d1559bf419e2c5d5ac73f71990a18
eshizhan <eshizhan@126.com>
2017-09-09 19:29:06 +0800

    porting to python 3, just work only
    Signed-off-by: eshizhan <eshizhan@126.com>

--- plugins/dstat_freespace.py
+++ plugins/dstat_freespace.py
@@ -17,7 +17,7 @@ class dstat_plugin(dstat):
         ret = []
         for l in self.splitlines():
             if len(l) < 6: continue
-            if l[2] in ('binfmt_misc', 'devpts', 'iso9660', 'none', 'proc', 'sysfs', 'usbfs', 'cgroup', 'tmpfs', 'devtmpfs', 'debugfs', 'mqueue', 'systemd-1', 'rootfs', 'autofs'): continue
+            if l[2] in ('binfmt_misc', 'devpts', 'iso9660', 'none', 'proc', 'sysfs', 'usbfs', 'cgroup', 'tmpfs', 'devtmpfs', 'debugfs', 'mqueue', 'systemd-1', 'rootfs', 'autofs', 'overlay', 'nsfs'): continue
             ### FIXME: Excluding 'none' here may not be what people want (/dev/shm)
             if l[0] in ('devpts', 'none', 'proc', 'sunrpc', 'usbfs', 'securityfs', 'hugetlbfs', 'configfs', 'selinuxfs', 'pstore', 'nfsd'): continue
             name = l[1]
@@ -35,7 +35,7 @@ class dstat_plugin(dstat):
         self.val['total'] = (0, 0)
         for name in self.vars:
             res = os.statvfs(name)
-            self.val[name] = ( (float(res.f_blocks) - float(res.f_bavail)) * long(res.f_frsize), float(res.f_bavail) * float(res.f_frsize) )
+            self.val[name] = ( (float(res.f_blocks) - float(res.f_bavail)) * int(res.f_frsize), float(res.f_bavail) * float(res.f_frsize) )
             self.val['total'] = (self.val['total'][0] + self.val[name][0], self.val['total'][1] + self.val[name][1])
 
 # vim:ts=4:sw=4:et
